8f74d31c14ae4c9d73b9e656e3bc7c50a8953b19
Sners03 <reichelarslukas@gmail.com>
2020-02-25 22:32:20 +0100

    added a recursive Version of Bubblesort

--- recursive_Bubblesort.py
+++ recursive_Bubblesort.py
@@ -1,3 +1,14 @@
-liste=[1,2,3,5,4]
+def recursive_Bubblesort(Liste):
+    for i, num in enumerate(Liste): 
+        try: 
+            if Liste[i+1] < num: 
+                Liste[i] = Liste[i+1] 
+                Liste[i+1] = num 
+                recursive_Bubblesort(Liste)
+        except IndexError:
+            pass
+    return Liste 
 
-print(list(enumerate(liste)))
+if __name__ == '__main__':
+    Liste1= [2,3,1,4,0]
+    print(recursive_Bubblesort(Liste1))
